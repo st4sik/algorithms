233a1b46249bd5a635af22cc860ba7f2e3a72b29
Stanislav Riabokon <Stanislav_Riabokon@epam.com>
2018-07-12 16:51:37 +0400

    Added some algorithms:
    * Monotonic shortest path -
    * Shortest path with one skippable edge
    * Second shortest path -

---
+++ part_2/week_2/shortest_paths/quiz/src/main/java/com/algo/quiz/MonotonicSP.java
@@ -0,0 +1,13 @@
+package com.algo.quiz;
+
+/**
+ * Monotonic shortest path. Given an edge-weighted digraph G, design an ElogE algorithm to find a
+ * monotonic shortest path from s to every other vertex. A path is monotonic if the sequence of edge
+ * weights along the path are either strictly increasing or strictly decreasing.
+ *
+ * Hint: relax edges in ascending order to find a best monotonically increasing path; relax edges in
+ * descending order to find a best monotonically decreasing path.
+ */
+public class MonotonicSP {
+
+}
---
+++ part_2/week_2/shortest_paths/quiz/src/main/java/com/algo/quiz/SecondShortestPath.java
@@ -0,0 +1,15 @@
+package com.algo.quiz;
+
+/**
+ * Second shortest path. Given an edge-weighted digraph and let P be a shortest path from vertex s
+ * to vertex t. Design an ElogV algorithm to find a path other than P from s to t that is as short
+ * as possible. Assume all of the edge weights are strictly positive.
+ *
+ * Hint: compute the shortest path distances from s to every vertex and the shortest path distances
+ * from every vertex to t.
+ */
+public class SecondShortestPath {
+
+  //Yen's algorithm
+
+}
