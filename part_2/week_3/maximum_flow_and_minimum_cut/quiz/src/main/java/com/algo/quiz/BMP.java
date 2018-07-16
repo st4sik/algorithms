b7da461b76d15f63e4330d61c8b38976d0e7a03a
Stanislav Riabokon <Stanislav_Riabokon@epam.com>
2018-07-16 12:10:18 +0400

    Added some algorithms: * Perfect matchings in k-regular bipartite graphs - * Fattest path - * Maximum weight closure problem -

---
+++ part_2/week_3/maximum_flow_and_minimum_cut/quiz/src/main/java/com/algo/quiz/BMP.java
@@ -0,0 +1,17 @@
+package com.algo.quiz;
+
+/**
+ * Perfect matchings in k-regular bipartite graphs. Suppose that there are n men and n women at a
+ * dance and that each man knows exactly k women and each woman knows exactly k men (and
+ * relationships are mutual). Show that it is always possible to arrange a dance so that each man
+ * and woman are matched with someone they know.
+ *
+ * Hint: formulate the bipartite matching problem as a maxflow problem; find a (fractional) feasible
+ * flow of value n; conclude that there is a perfect matching.
+ */
+public class BMP {
+
+  //https://www.geeksforgeeks.org/
+  //https://algs4.cs.princeton.edu/65reductions/BipartiteMatching.java
+  //http://www.cs.princeton.edu/~wayne/cs423/lectures/max-flow-applications
+}
---
+++ part_2/week_3/maximum_flow_and_minimum_cut/quiz/src/main/java/com/algo/quiz/MaxWeightClosureProblem.java
@@ -0,0 +1,13 @@
+package com.algo.quiz;
+
+/**
+ * Maximum weight closure problem. A subset of vertices S in a digraph is closed if there are no
+ * edges pointing from S to a vertex outside S. Given a digraph with weights (positive or negative)
+ * on the vertices, find a closed subset of vertices of maximum total weight.
+ *
+ * Hint: formulate as a mincut problem; assign edge (v, w)(v,w) a weight of infinity if there is an
+ * edge from v to w in the original digraph.
+ */
+public class MaxWeightClosureProblem {
+  //https://en.wikipedia.org/wiki/Closure_problem
+}
