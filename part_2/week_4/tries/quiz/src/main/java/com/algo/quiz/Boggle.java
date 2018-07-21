bc714ee032a598c3d1a6b7783af169eeaf3742dd
st4sik <ryabokons@mail.ru>
2018-07-21 21:28:37 +0400

    Added some algorithms: * Boogle - * Prefix Free Codes * Suffix Trees

---
+++ part_2/week_4/tries/quiz/src/main/java/com/algo/quiz/Boggle.java
@@ -0,0 +1,13 @@
+package com.algo.quiz;
+
+/**
+ * Boggle. Boggle is a word game played on an 44-by-44 grid of tiles, where each tile contains one
+ * letter in the alphabet. The goal is to find all words in the dictionary that can be made by
+ * following a path of adjacent tiles (with no tile repeated), where two tiles are adjacent if they
+ * are horizontal, vertical, or diagonal neighbors.
+ *
+ * Hint: create a trie containing all of the words in the dictionary.
+ */
+public class Boggle {
+
+}
