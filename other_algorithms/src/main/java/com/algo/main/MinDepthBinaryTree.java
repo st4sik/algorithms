5b9e1c590ac314532e9a11e55746db1008e1886f
Stanislav Riabokon <Stanislav_Riabokon@epam.com>
2018-10-16 14:17:39 +0400

    Added a new algo 'MinDepthBinaryTree'

---
+++ other_algorithms/src/main/java/com/algo/main/MinDepthBinaryTree.java
@@ -0,0 +1,33 @@
+package com.algo.main;
+
+public class MinDepthBinaryTree {
+
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public int minDepth(TreeNode root) {
+		if (root == null) {
+			return 0;
+		}
+		if (root.left == null && root.right == null) {
+			return 1;
+		}
+		if (root.left == null) {
+			return 1 + minDepth(root.right);
+		}
+		if (root.right == null) {
+			return 1 + minDepth(root.left);
+		}
+		return 1 + Math.min(minDepth(root.left), minDepth(root.right));
+	}
+
+	public static void main(String[] args) {
+	}
+}
